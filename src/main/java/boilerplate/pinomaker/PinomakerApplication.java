f39491946a3e54c575b9ecb6a18618809d5edc21
pinomaker-hoo <inhoo23@naver.com>
2023-03-01 21:30:20 +0900

    Init : Create Project

---
+++ src/main/java/boilerplate/pinomaker/PinomakerApplication.java
@@ -0,0 +1,13 @@
+package boilerplate.pinomaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinomakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinomakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/boilerplate/pinomaker/PinomakerApplicationTests.java
@@ -0,0 +1,13 @@
+package boilerplate.pinomaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinomakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
