45200c966045514f0c31dac7d3d1e358ad20da52
pinomaker-hoo <inhoo23@naver.com>
2023-09-21 18:10:29 +0900

    feat : Add Scheduling

---
+++ src/main/java/boilerplate/pinomaker/scheduling/PushScheduling.java
@@ -0,0 +1,16 @@
+package boilerplate.pinomaker.scheduling;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class PushScheduling {
+    @Scheduled(cron = "* 0 * * * ?", zone = "Asia/Seoul")
+    public void scheduleTaskUsingCronExpression() {
+        long now = System.currentTimeMillis() / 1000;
+        log.info("schedule tasks using cron jobs - {}", now);
+    }
+}
