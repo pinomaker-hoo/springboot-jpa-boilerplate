9a75ca181749c252f8afd783f3f855de3c871af5
pinomaker-hoo <inhoo23@naver.com>
2023-10-30 12:59:26 +0900

    fix : Generate Token

--- src/main/java/boilerplate/pinomaker/api/auth/service/Impl/AuthServiceImpl.java
+++ src/main/java/boilerplate/pinomaker/api/auth/service/Impl/AuthServiceImpl.java
@@ -8,16 +8,12 @@ import boilerplate.pinomaker.global.dto.CommonResponse;
 import boilerplate.pinomaker.global.dto.TokenDto;
 import boilerplate.pinomaker.global.exception.BadRequestException;
 import boilerplate.pinomaker.global.exception.NotFoundException;
-import boilerplate.pinomaker.global.jwt.TokenProvider;
+import boilerplate.pinomaker.global.jwt.JwtTokenProvider;
 import boilerplate.pinomaker.api.user.repository.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
 import boilerplate.pinomaker.global.dto.UserAuthority;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -29,8 +25,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final UserJpaRepository userJpaRepository;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
-    private final TokenProvider tokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -64,11 +59,7 @@ public class AuthServiceImpl implements AuthService {
             throw new BadRequestException("비밀번호가 같지 않습니다.");
         }
 
-        UsernamePasswordAuthenticationToken authenticationToken = dto.toAuthentication();
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        TokenDto tokenDto = tokenProvider.generateTokenDto(authenticationToken, "USER");
+        TokenDto tokenDto = jwtTokenProvider.issueToken(findUser.get().getIdx(), 3600L, 3600L);
 
         Map<String, String> response = new HashMap<>();
         response.put("accessToken", tokenDto.getAccessToken());
